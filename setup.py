69bd24acb4fe9e32672ad5799fd0097232f73cf5
Ben Fox <benmfox@me.com>
2024-07-20 08:47:43 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit_lexical",
-    version="0.2",
+    version="0.3",
     author="Ben Fox",
     author_email="ben@musubilabs.ai",
     description="Streamlit component that allows you to use Meta's Lexical rich text editor",
